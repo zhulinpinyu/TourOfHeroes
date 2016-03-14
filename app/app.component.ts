f650cc629f3879a15cf66706a18f293eeea32013
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-14 14:14:24 +0800

    双向绑定

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,26 @@
 import {Component} from 'angular2/core';
 
+interface Hero{
+  id: number;
+  name: String;
+}
+
 @Component({
   selector: 'my-app',
-  template: "<h2>Hi Angular2 App!</h2>"
+  template: `<h1>{{hero.name}}</h1>
+             <h2>{{title}}</h2>
+             <div><label>ID:</label>{{hero.id}}</div>
+             <div>
+                <label>Name:</label>
+                <div><input [(ngModel)]="hero.name" placehoder="name"/></div>
+             </div>`
 })
-export class AppComponent{}
+export class AppComponent{
+  public title = "Tour of Heroes!";
+  public name = "Demo";
+
+  public hero:Hero = {
+    id: 1,
+    name: "Window son!"
+  }
+}
