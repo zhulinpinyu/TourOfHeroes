5dc312f59e610ab87eb31ad15f5a271de9e877b3
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-14 14:40:08 +0800

    list hero and click show hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,22 +5,96 @@ interface Hero{
   name: String;
 }
 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
 @Component({
   selector: 'my-app',
-  template: `<h1>{{hero.name}}</h1>
-             <h2>{{title}}</h2>
-             <div><label>ID:</label>{{hero.id}}</div>
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 10em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }`],
+  template: `<h1>{{title}}</h1>
+             <h2>{{name}}</h2>
+             <ul class="heroes">
+                <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero===selectedHero">
+                  <span class="badge">{{hero.id}}</span>{{hero.name}}
+                </li>
+             </ul>
+             <div *ngIf="selectedHero">
+               <h2>{{selectedHero.name}}</h2>
+               <div>
+                  <label>id:</label>{{selectedHero.id}}
+               </div>
+               <div>
+                  <label>name:</label>
+                  <div>
+                    <input [(ngModel)]="selectedHero.name" placeHolder="name"/>
+                  </div>
+               </div>
              <div>
-                <label>Name:</label>
-                <div><input [(ngModel)]="hero.name" placehoder="name"/></div>
-             </div>`
+             `
 })
 export class AppComponent{
   public title = "Tour of Heroes!";
-  public name = "Demo";
-
-  public hero:Hero = {
-    id: 1,
-    name: "Window son!"
+  public name = "My Heroes";
+  public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
   }
 }
