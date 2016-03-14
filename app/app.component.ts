7bf76d679f53431277fcfafd68994e04336873fe
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-14 17:40:37 +0800

    add dashboard nav link

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,14 @@ import {Component} from "angular2/core"
 import {RouteConfig,ROUTER_DIRECTIVES,ROUTER_PROVIDERS} from "angular2/router"
 import {HeroService} from "./hero.service"
 import {HeroesComponent} from "./heroes.component"
+import {DashboardComponent} from "./dashboard.component"
 
 @RouteConfig([{
+  path: "/dashboard",
+  name: "Dashboard",
+  component: DashboardComponent,
+  useAsDefault: true
+},{
   path: "/heroes",
   name: "Heroes",
   component: HeroesComponent
@@ -14,6 +20,7 @@ import {HeroesComponent} from "./heroes.component"
   template: `
     <h1>{{title}}</h1>
     <h2>{{name}}</h2>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
     <a [routerLink]="['Heroes']">Heroes</a>
     <router-outlet></router-outlet>
   `,
